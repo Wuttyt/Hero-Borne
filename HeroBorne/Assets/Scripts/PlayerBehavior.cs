e5d2b52a92764b67268f4637fd6d441340940fa5
Wuttyt <dfernandezvaler0318@gmail.com>
2024-05-10 19:11:53 -0400

    Added Jumping!!

--- HeroBorne/Assets/Scripts/PlayerBehavior.cs
+++ HeroBorne/Assets/Scripts/PlayerBehavior.cs
@@ -11,19 +11,31 @@ public class PlayerBehavior : MonoBehaviour
 
     private Rigidbody _rb;
 
+    //Time to add jumping!!
+    public float JumpVelocity = 5f;
+    private bool _isJumping;
+
+    public float DistanceToGround = 0.1f;
+    public LayerMask GroundLayer;
+    private CapsuleCollider _col;
+
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
+
+        _col = GetComponent<CapsuleCollider>();
     }
-
-
 
     // Update is called once per frame
     void Update()
     {
-        
+        _isJumping |= Input.GetKeyDown(KeyCode.Space);
+
         _vInput = Input.GetAxis("Vertical") * MoveSpeed;
         _hInput = Input.GetAxis("Horizontal") * RotateSpeed;
+
+        this.transform.Translate(Vector3.forward * _vInput * Time.deltaTime);
+        this.transform.Rotate(Vector3.up * _hInput * Time.deltaTime);
 
         /*
         this.transform.Translate(Vector3.forward * _vInput * Time.deltaTime);
@@ -35,12 +47,20 @@ public class PlayerBehavior : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector3 rotation = Vector3.up * _hInput;
-        
-        Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+        if(IsGrounded() && _isJumping)
+        {
+            _rb.AddForce(Vector3.up * JumpVelocity, ForceMode.Impulse);
+        }
 
-        _rb.MovePosition(this.transform.position + this.transform.forward * _vInput * Time.fixedDeltaTime);
+        _isJumping = false;
+    }
 
-        _rb.MoveRotation(_rb.rotation * angleRot);
+    private bool IsGrounded()
+    {
+        Vector3 capsuleBottom = new Vector3(_col.bounds.center.x, _col.bounds.min.y, _col.bounds.center.z);
+
+        bool grounded = Physics.CheckCapsule(_col.bounds.center,  capsuleBottom, DistanceToGround, GroundLayer, QueryTriggerInteraction.Ignore);
+
+        return grounded;
     }
 }
