b07d7a06992d109c6174c2abe88b847fca6479f2
Wuttyt <dfernandezvaler0318@gmail.com>
2024-05-10 21:22:08 -0400

    Added Teleportation Between Levels

---
+++ HeroBorne/Assets/Scripts/ChangeLevel.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ChangeLevel : MonoBehaviour
+{
+    int buildindex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buildindex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider myCollision)
+    {
+        SceneManager.LoadScene(buildindex + 1);
+    }
+}
